46ad3345a46a3962af2018f30b2c02b32297ff91
Alex Patin <alex@mona.gallery>
2022-07-17 18:05:11 -0400

    Updated build output

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ import CryptoAvatars from '@/providers/CryptoAvatars'
 import Meebits from '@/providers/Meebits'
 import ReadyPlayerMe, { ReadyPlayerMeMetadata } from '@/providers/ReadyPlayerMe'
 
-const providers = [CryptoAvatars, Meebits, ReadyPlayerMe]
+const providers = [CryptoAvatars, Meebits, ReadyPlayerMe].reduce<
+  Record<string, Provider<unknown>>
+>((acc, provider) => ({ ...acc, [provider.id]: provider }), {})
 
-export default providers.reduce<Record<string, Provider<unknown>>>(
-  (acc, provider) => ({ ...acc, [provider.id]: provider }),
-  {}
-)
+export default providers
 
 export interface ProviderMetadata {
   'ready-player-me': ReadyPlayerMeMetadata
+  meebits: null
+  'crypto-avatars': null
 }
